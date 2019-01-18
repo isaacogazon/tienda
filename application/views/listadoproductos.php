a226791062b073d5792cdf987d367f31407d8be1
Isaac Ogazón Pichardo <isaac_op_92@hotmail.com>
2019-01-18 08:46:53 +0100

    Añadiento el estilo con el cabecero, menu lateral y footer.

---
+++ application/views/listadoproductos.php
@@ -0,0 +1,26 @@
+
+        <!-- =============================================== -->
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <h1>
+                Productos
+                <small>Listado</small>
+                </h1>
+            </section>
+            <!-- Main content -->
+            <section class="content">
+                <!-- Default box -->
+                <div class="box box-solid">
+                    <div class="box-body">
+                    Aqui iran todos los productos
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
